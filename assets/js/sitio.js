ca995ddf2ebd4957058f6c7a70871b506aa5f0f5
MiguelUFV <9302286@alumnos.ufv.es>
2026-09-24 02:33:49 +0300

    Publicar web desde n8n-agent-audit @ b0a79c1

--- assets/js/sitio.js
+++ assets/js/sitio.js
@@ -785,7 +785,7 @@
       window.dataLayer = window.dataLayer || [];
       window.gtag = function gtag() { window.dataLayer.push(arguments); };
       window.gtag('js', new Date());
-      window.gtag('config', GA_ID);
+      window.gtag('config', GA_ID, { allow_google_signals: false, allow_ad_personalization_signals: false });
       const script = document.createElement('script');
       script.async = true;
       script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
